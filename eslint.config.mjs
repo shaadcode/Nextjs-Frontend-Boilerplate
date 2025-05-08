ad67cad4dbf8fd3038229077766b8b6705b29e0f
seyed <shaadcode@gmail.come>
2025-05-08 15:54:51 +0330

    feat(deps): add devtools for tanstack query

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,6 @@ import jestDom from 'eslint-plugin-jest-dom';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
 import playwright from 'eslint-plugin-playwright';
 import testingLibrary from 'eslint-plugin-testing-library';
-
 const ignoreFiles = [
   './README.md',
   'migrations/**/*',
@@ -30,6 +29,7 @@ export default antfu(
 
     // Ignored paths
     ignores: ignoreFiles,
+
   },
   // --- Next.js Specific Rules ---
   {
@@ -57,6 +57,7 @@ export default antfu(
   // --- Custom Rule Overrides ---
   {
     rules: {
+
       'antfu/no-top-level-await': 'off', // Allow top-level await
       'style/brace-style': ['error', '1tbs'], // Use the default brace style
       'ts/consistent-type-definitions': ['error', 'type'], // Use `type` instead of `interface`
