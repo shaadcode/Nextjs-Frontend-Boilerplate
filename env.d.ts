d5cc8099f56f5ef2c05e763679a759f65e30d81a
seyed <shaadcode@gmail.come>
2025-05-08 14:17:04 +0330

    feat(config): basic T3 env config

--- env.d.ts
+++ env.d.ts
@@ -1,10 +1,12 @@
 declare namespace NodeJS {
   // eslint-disable-next-line ts/consistent-type-definitions
   interface ProcessEnv {
-    ENV_APP: 'development' | 'production' | 'test';
+    NODE_ENV: 'development' | 'production' | 'test';
     ANALYZE: 'true' | 'false';
     CI: boolean;
     PORT: number;
     NEXT_PUBLIC_BASE_API_URL: string;
+
+    SKIP_ENV_VALIDATION: 'true' | 'false' | undefined;
   }
 }
--- src/env.ts
+++ src/env.ts
@@ -1,14 +1,24 @@
+// env.ts
 import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
 
 export const env = createEnv({
-  server: {
 
+  server: {
+    NODE_ENV: z.enum(['development', 'production', 'test']),
+    PORT: z.coerce.number().optional(),
+    ANALYZE: z.enum(['true', 'false']).optional(),
+    CI: z.enum(['true', 'false']).optional(),
   },
   client: {
-
+    NEXT_PUBLIC_BASE_API_URL: z.string().url(),
   },
-  // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
   runtimeEnv: {
-
+    NODE_ENV: process.env.NODE_ENV,
+    PORT: process.env.PORT,
+    ANALYZE: process.env.ANALYZE,
+    CI: process.env.CI,
+    NEXT_PUBLIC_BASE_API_URL: process.env.NEXT_PUBLIC_BASE_API_URL,
   },
+  skipValidation: process.env.SKIP_ENV_VALIDATION === 'true',
 });
