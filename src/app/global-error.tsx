ce6d4d725c8f696e510325ed245b51b190a78aa8
seyed <shaadcode@gmail.come>
2025-05-10 17:05:14 +0330

    feat(config): add global-error file to the root of the application

---
+++ src/app/global-error.tsx
@@ -0,0 +1,56 @@
+// You cannot use Next-Intl in this file
+
+'use client';
+import routing from '@/config/i18n/routing';
+
+import { theme } from '@/config/mantine';
+import { Button, Container, Group, MantineProvider, Paper, Stack, Text, Title } from '@mantine/core';
+import { IconAlertTriangle, IconHome } from '@tabler/icons-react';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+import '@mantine/core/styles.css';
+
+export default function GlobalError(props: {
+  error: Error & { digest?: string; message: string };
+  reset: () => void;
+}) {
+  const router = useRouter();
+
+  useEffect(() => {
+    console.error('Global Error:', props.error);
+  }, [props.error]);
+  return (
+    <html lang={routing.defaultLocale}>
+      <body>
+        <MantineProvider theme={theme}>
+          <Container size="sm" pt={100}>
+            <Paper shadow="xl" p="xl" withBorder radius="md">
+              <Stack gap="md" align="center">
+                <IconAlertTriangle size={48} color="red" />
+                <Title order={2}>
+                  An error has occurred
+                </Title>
+                <Text c="dimmed" ta="center">
+                  {props.error.message || 'An unexpected error has occurred. Please try again or return home.'}
+                </Text>
+
+                <Group align="center" mt="md">
+                  <Button variant="filled" color="red" onClick={() => props.reset()}>
+                    Retry
+                  </Button>
+                  <Button
+                    variant="outline"
+                    leftSection={<IconHome size={18} />}
+                    onClick={() => router.push('/')}
+                  >
+                    Return Home
+                  </Button>
+                </Group>
+              </Stack>
+            </Paper>
+          </Container>
+        </MantineProvider>
+      </body>
+    </html>
+  );
+}
