292eab0d2b99d0d1744ea7d5d99b8545b7c019a5
shaadcode <shaadcode@gmail.com>
2025-05-20 15:34:52 +0330

    feat(config): adding autoRem functionality to postCSS

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -8,7 +8,9 @@
 const config = {
   plugins: {
     '@tailwindcss/postcss': {},
-    'postcss-preset-mantine': {},
+    'postcss-preset-mantine': {
+      autoRem: true,
+    },
     'postcss-simple-vars': {
       variables: {
         'mantine-breakpoint-xs': '320px',
