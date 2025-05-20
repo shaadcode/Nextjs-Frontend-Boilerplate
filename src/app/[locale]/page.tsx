4ac7f5bb1c34657bd14997203e8dc835d0cf8e45
shaadcode <shaadcode@gmail.com>
2025-05-20 17:52:52 +0330

    chore: minor changes to the home page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,16 +1,17 @@
-import { Button } from '@mantine/core';
+import { Button, Center } from '@mantine/core';
 import { useTranslations } from 'next-intl';
 
 const Home = () => {
   const t = useTranslations('HomePage');
+
   return (
-    <>
+    <Center pt="lg">
 
       <Button lang="fa">
         {t('title')}
       </Button>
 
-    </>
+    </Center>
 
   );
 };
