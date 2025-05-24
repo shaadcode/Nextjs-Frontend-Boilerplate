7e5f0f8ca3cfae76cfd55184320d060002d6dee0
shaadcode <shaadcode@gmail.com>
2025-05-24 13:25:46 +0330

    feat(config): add restriction on not using regular strings in jsx

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -91,6 +91,10 @@ export default antfu(
           },
         ],
       }],
+      'style/jsx-curly-brace-presence': ['error', {
+        props: 'ignore',
+        children: 'always',
+      }],
       'antfu/no-top-level-await': 'off', // Allow top-level await
       'style/brace-style': ['error', '1tbs'], // Use the default brace style
       'ts/consistent-type-definitions': ['error', 'type'], // Use `type` instead of `interface`
