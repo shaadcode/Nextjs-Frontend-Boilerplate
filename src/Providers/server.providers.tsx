44be89fa64e73ee5d95f0caa55371aca9a93daf8
shaadcode <shaadcode@gmail.com>
2025-05-28 10:30:13 +0330

    feat(deps): add the mantine notifications library

--- src/Providers/server.providers.tsx
+++ src/Providers/server.providers.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable perfectionist/sort-imports */
 import '@mantine/core/styles.css';
+import '@mantine/notifications/styles.css';
 
 import { cssVariablesResolver, theme } from '@/config/mantine/mantine';
 import { DirectionProvider, MantineProvider } from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
 import { NextIntlClientProvider } from 'next-intl';
 import type { ReactNode } from 'react';
 
@@ -14,6 +16,8 @@ const ServerProviders = ({ children }: { children: ReactNode }) => {
         cssVariablesResolver={cssVariablesResolver}
         theme={theme}
       >
+        <Notifications containerWidth={360} position="top-right" />
+
         <NextIntlClientProvider>
           {children}
         </NextIntlClientProvider>
